888d578dc4a0fedbe2874ae58e6c4473011100d2
davorin <47928470+davorinpiljic@users.noreply.github.com>
2020-10-04 18:04:01 -0600

    merge sort

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -22,7 +22,6 @@ def binary_search(arr, target, start, end):
         print(start, end, arr, mid)
         return binary_search(arr, target, start, end)
 
-
         # STRETCH: implement an order-agnostic binary search
         # This version of binary search should correctly find
         # the target regardless of whether the input array is
@@ -33,5 +32,5 @@ arr = [-9, -8, -6, -4, -3, -2, 0, 1, 2, 3, 5, 7, 8, 9]
 binary_search(arr, 1, 0, len(arr))
 
 
-# def agnostic_binary_search(arr, target):
-# Your code here
+def agnostic_binary_search(arr, target):
+    # Your code here
--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -5,24 +5,58 @@ def merge(arrA, arrB):
 
     # Your code here
 
-
     return merged_arr
 
 # TO-DO: implement the Merge Sort function below recursively
+
+
 def merge_sort(arr):
     # Your code here
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        left = arr[:mid]
+        right = arr[mid:]
+
+        merge_sort(left)
+        merge_sort(right)
+
+        i = 0
+        j = 0
+        m = 0
+
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                arr[m] = left[i]
+                i += 1
+            else:
+                arr[m] = right[j]
+                j += 1
+            m += 1
+
+        while i < len(left):
+            arr[m] = left[i]
+            i += 1
+            m += 1
+        while j < len(right):
+            arr[m] = right[j]
+            j += 1
+            m += 1
 
 
-    return arr
+arr = [1, 5, 8, 4, 2, 9, 6, 0, 3, 7]
+print(arr)
+merge_sort(arr)
+print(arr)
 
-# STRETCH: implement the recursive logic for merge sort in a way that doesn't 
+# STRETCH: implement the recursive logic for merge sort in a way that doesn't
 # utilize any extra memory
-# In other words, your implementation should not allocate any additional lists 
+# In other words, your implementation should not allocate any additional lists
 # or data structures; it can only re-use the memory it was given as input
-def merge_in_place(arr, start, mid, end):
-    # Your code here
 
 
-def merge_sort_in_place(arr, l, r):
-    # Your code here
+# def merge_in_place(arr, start, mid, end):
+# Your code here
 
+
+# def merge_sort_in_place(arr, l, r):
+# Your code here
